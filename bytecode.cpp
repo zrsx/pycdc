1cfcb00cf5f4785492b930b84fd706417ec17f96
𝗚𝗘𝗢𝗥𝗚𝗘 <george.methq@gmail.com>
2025-08-31 13:21:28 +0600

    Address comments

--- bytecode.cpp
+++ bytecode.cpp
@@ -603,17 +603,28 @@ void bc_disasm(std::ostream& pyc_output, PycRef<PycCode> code, PycModule* mod,
     }
 }
 
-void bc_exceptiontable(std::ostream& pyc_output, PycRef<PycCode> code,
-               int indent)
+// Print exception table entries using tuple-style (preferred for new code)
+void bc_exceptiontable(std::ostream& pyc_output, PycRef<PycCode> code, int indent)
 {
     for (auto tuple: code->exceptTableEntries()) {
-
         for (int i=0; i<indent; i++)
             pyc_output << "    ";
-
         pyc_output << std::get<0>(tuple) << " to " << std::get<1>(tuple);
         pyc_output << " -> " << std::get<2>(tuple) << " ";
         pyc_output << "[" << std::get<3>(tuple) << "] " << (std::get<4>(tuple) ? "lasti": "");
         pyc_output << "\n";
+    }
+}
+
+// Optionally, support the old-style class for legacy compatibility
+void bc_exceptiontable_entries(std::ostream& pyc_output, PycRef<PycCode> code, int indent)
+{
+    for (const auto& entry : code->exceptionTableEntries()) {
+        for (int i=0; i<indent; i++)
+            pyc_output << "    ";
+        pyc_output << entry.start_offset << " to " << entry.end_offset
+                   << " -> " << entry.target << " [" << entry.stack_depth
+                   << "] " << (entry.push_lasti ? "lasti": "")
+                   << "\n";
     }
 }
